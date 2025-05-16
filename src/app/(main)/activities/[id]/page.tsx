16ea2941ddc75dab5426cc3f638f74231093822e
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 21:30:23 +0000

    feat(activities): Implement activity page with back navigation

--- src/app/(main)/activities/[id]/page.tsx
+++ src/app/(main)/activities/[id]/page.tsx
@@ -1,7 +1,22 @@
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+"use client";
 
-export default Page
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  const router = useRouter();
+
+  return (
+    <div className="px-8 py-4 space-y-4">
+      <div>
+        <Button size="sm" title="Back" onClick={() => router.back()}>
+          <ArrowLeft />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
