582996c55bb0b5e32b59aa65fb840cd515e3a2f5
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 19:17:28 +0000

    feat(activities): Implement activities fetching with loading state

--- src/app/(main)/activities/page.tsx
+++ src/app/(main)/activities/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import useActivities from "./hooks/use-activities";
+import Loading from "@/app/loading";
+
 const Page = () => {
-  return <div>Activities</div>;
+  const { activitiesQuery } = useActivities();
+  const activities = activitiesQuery.data || [];
+
+  if (activitiesQuery.isPending) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <h1>Activities</h1>
+      {activities.map((activity) => (
+        <div key={activity.id}>
+          <h2>{activity.name}</h2>
+          <ul>
+            {activity.scores.map((score) => (
+              <li key={score.id}>{score.value}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
