b59a7d781c4fc5e16a05b285ac0d5f8721a2c77b
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 19:14:33 +0000

    refactor(query-functions): Remove console log from fetchActivities

--- src/app/(main)/activities/utils/query-functions.ts
+++ src/app/(main)/activities/utils/query-functions.ts
@@ -4,8 +4,6 @@ export const fetchActivities = async () => {
   try {
     const response = await axios.get("/activities");
 
-    console.log("Response data:", response.data);
-
     return response.data;
   } catch (error) {
     console.error("Error from `readProducts`:", error);
