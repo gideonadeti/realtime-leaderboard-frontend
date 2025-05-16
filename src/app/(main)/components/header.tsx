f5ef589a913739e4a85c6dcc6e8d118ecfa1dfc9
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 16:30:42 +0000

    style(header): Remove sticky positioning from header

--- src/app/(main)/components/header.tsx
+++ src/app/(main)/components/header.tsx
@@ -7,7 +7,7 @@ import { H5 } from "@/components/ui/typography";
 
 const Header = () => {
   return (
-    <header className="flex items-center ps-2 pe-4 py-2 border-b sticky top-0 z-10 bg-white">
+    <header className="flex items-center ps-2 pe-4 py-2 border-b">
       <SidebarTrigger />
       <Separator orientation="vertical" className="mx-2" />
       <Link href="/activities">
