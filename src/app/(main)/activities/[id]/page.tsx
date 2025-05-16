76aefb0d9fc00ae2992f53f2f8d54020538771fb
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 21:37:53 +0000

    feat(activities): Enhance activity page with loading state and data retrieval

--- src/app/(main)/activities/[id]/page.tsx
+++ src/app/(main)/activities/[id]/page.tsx
@@ -1,19 +1,37 @@
 "use client";
 
+import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
+import useActivities from "../hooks/use-activities";
+import Loading from "@/app/loading";
 import { Button } from "@/components/ui/button";
+import { H3 } from "@/components/ui/typography";
 
 const Page = () => {
   const router = useRouter();
+  const { id } = useParams();
+  const { activitiesQuery } = useActivities();
+  const activity = activitiesQuery.data?.find((a) => a.id === id);
+
+  if (activitiesQuery.isPending) {
+    return <Loading />;
+  }
 
   return (
     <div className="px-8 py-4 space-y-4">
-      <div>
-        <Button size="sm" title="Back" onClick={() => router.back()}>
-          <ArrowLeft />
-        </Button>
+      <Button size="sm" title="Back" onClick={() => router.back()}>
+        <ArrowLeft />
+      </Button>
+      <div className="flex items-center justify-between">
+        <H3>{`${activity?.name}'s scores`}</H3>
+        <Link
+          href={`/leaderboards/${activity?.id}`}
+          className="hover:underline hover:text-blue-700"
+        >
+          Leaderboard
+        </Link>
       </div>
     </div>
   );
