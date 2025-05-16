3e285726acee14535311e163e9cc13aab9f2db07
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-16 16:54:32 +0000

    feat(header): Enhance UserButton with theme support

--- src/app/(main)/components/header.tsx
+++ src/app/(main)/components/header.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
 
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import { H5 } from "@/components/ui/typography";
 
 const Header = () => {
+  const { theme, systemTheme } = useTheme();
+
   return (
     <header className="flex items-center ps-2 pe-4 py-2 border-b">
       <SidebarTrigger />
@@ -14,7 +17,18 @@ const Header = () => {
         <H5>Real-time Leaderboard</H5>
       </Link>
       <div className="ms-auto">
-        <UserButton showName />
+        <UserButton
+          appearance={{
+            elements: {
+              userButtonOuterIdentifier: `${
+                theme === "light" || systemTheme === "light"
+                  ? ""
+                  : "!text-white"
+              }`,
+            },
+          }}
+          showName
+        />
       </div>
     </header>
   );
