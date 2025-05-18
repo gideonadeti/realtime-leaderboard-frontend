3d1845c62274491e7ca639582903654d6e74eaeb
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-18 13:26:47 +0000

    refactor(get-ordinal): Remove explicit return type annotation

--- src/app/(main)/utils/get-ordinal.ts
+++ src/app/(main)/utils/get-ordinal.ts
@@ -1,4 +1,4 @@
-const getOrdinal = (number: number): string => {
+const getOrdinal = (number: number) => {
   const lastDigit = number % 10;
   const lastTwoDigits = number % 100;
   const isTeens = lastTwoDigits >= 11 && lastTwoDigits <= 13;
