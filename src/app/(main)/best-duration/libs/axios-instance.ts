c71c456286c212e8521ccbb72b7c0d65bbf5145d
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-20 21:39:00 +0000

    chore(leaderboards): Remove unused leaderboard hooks, axios, and socket instances
    - Delete `useLeaderboards` hook, axios instance, socket instance, and related query functions to clean up the codebase.
    - This removal streamlines the project by eliminating redundant code and unused imports.

--- src/app/(main)/best-duration/libs/axios-instance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1"
-  }/games`,
-});
-
-export default axiosInstance;
