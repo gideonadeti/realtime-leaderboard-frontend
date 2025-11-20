071395213e1883c73bd6c9f9f0433909a4076715
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-20 21:17:19 +0000

    feat(socket): Add socket instance for real-time communication and update axios base URL for API versioning

--- src/app/(main)/best-duration/libs/axios-instance.ts
+++ src/app/(main)/best-duration/libs/axios-instance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "http://localhost:3000/api/v1/games",
+  baseURL: `${
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1"
+  }/games`,
 });
 
 export default axiosInstance;
