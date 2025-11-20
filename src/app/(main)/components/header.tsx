6be8f48d52b414c9cd7e89fd315c155226480688
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-20 21:58:32 +0000

    refactor(header): Simplify header component by removing unused elements and updating link
    - Remove UserButton and ThemeToggler components for a cleaner header structure.
    - Update the link from "Activities" to "Game Count" for improved navigation.

--- src/app/(main)/components/header.tsx
+++ src/app/(main)/components/header.tsx
@@ -1,38 +1,17 @@
 import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
-import { useTheme } from "next-themes";
 
-import { ThemeToggler } from "./theme-toggler";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import { H5 } from "@/components/ui/typography";
 
 const Header = () => {
-  const { theme, systemTheme } = useTheme();
-
   return (
     <header className="flex items-center ps-2 pe-4 py-2 border-b sticky top-0 bg-background z-50">
       <SidebarTrigger />
       <Separator orientation="vertical" className="mx-2 !h-8" />
-      <Link href="/activities">
+      <Link href="/game-count">
         <H5>Real-time Leaderboard</H5>
       </Link>
-      <div className="ms-auto flex items-center">
-        <UserButton
-          appearance={{
-            elements: {
-              userButtonOuterIdentifier: `${
-                theme === "light" || systemTheme === "light"
-                  ? ""
-                  : "!text-white"
-              }`,
-            },
-          }}
-          showName
-        />
-        <Separator orientation="vertical" className="mx-2 !h-8" />
-        <ThemeToggler />
-      </div>
     </header>
   );
 };
