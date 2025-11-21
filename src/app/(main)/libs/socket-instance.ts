45170704740f40de31454887013d091b51d06552
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-21 19:34:13 +0000

    fix(socket-instance): Update Socket.IO client base URL to remove API versioning
    - Change the default base URL in socket-instance.ts from "/api/v1" to the root URL for improved compatibility with the API structure.

--- src/app/(main)/libs/socket-instance.ts
+++ src/app/(main)/libs/socket-instance.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socketInstance = io(
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1",
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
   {
     transports: ["websocket"],
   }
